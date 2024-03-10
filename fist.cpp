e39239a14ca5c6bf2ba7c3f26217b7d3cbb44441
Manuel Fuentes <rafaelgodinez275@gmail.com>
2024-03-10 08:15:18 -0600

    agregar clase AACP

--- fist.cpp
+++ fist.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
-class BRGQ{
+class AACCP{
 	private : string nombres,apellidos,direccion;
 	
 	void crear();
 	void leer();
 	void actualizar();
 	void borrar();
+	
 };
